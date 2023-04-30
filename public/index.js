05550284f4ae0a7f427b5c8bd43d9dda44041852
unknown <princeali4503050@gmail.com>
2023-04-30 20:25:20 +0500

    first comment from ali

---
+++ public/index.js
@@ -0,0 +1,20 @@
+const wrapper = document.querySelector(".wrapper"),
+qrInput = wrapper.querySelector(".form input");
+generateBtn = wrapper.querySelector(".form button");
+qrImg = wrapper.querySelector(".qr-code img");
+
+
+generateBtn.addEventListener("click",() => {
+    let qrValue = qrInput.value; 
+    if(!qrValue)return; // if the input is empty then return from here
+    generateBtn.innerText = "Generating QR Code..."
+// getting a QR code of user entered value using the qrserver 
+// api and passing the api returned img src to qrImg
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`
+// console.log(qrValue)
+qrImg.addEventListener("load",()=>{// once Qr code img loaded
+    wrapper.classList.add("active");
+    generateBtn.innerText = "Generating QR Code..."
+})
+    
+});
